d1853e645b3d20195289addf80d9b200767c2c35
Patrick Abele <patrick.abele@Patricks-Air.fritz.box>
2023-11-01 19:35:43 +0100

    more excercises

--- Chapter1/readInput.go
+++ Chapter1/readInput.go
@@ -46,10 +46,31 @@ func dup2() {
 	}
 	for line, n := range counts {
 		if n > 1 {
+			fmt.Printf("%s: %d\t%s\n", os.Args[1], n, line)
+		}
+	}
+}
+
+func dup3() {
+	counts := make(map[string]int)
+	for _, filename := range os.Args[1:] {
+		data, err := os.ReadFile(filename)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "dup3: %v\n", err)
+			continue
+		}
+		for _, line := range strings.Split(string(data), "\n") {
+			counts[line]++
+		}
+	}
+	for line, n := range counts {
+		if n > 1 {
 			fmt.Printf("%d\t%s\n", n, line)
 		}
 	}
 }
+
+//_______________HELPER__________________
 
 func countLines(f *os.File, counts map[string]int) {
 	input := bufio.NewScanner(f)
