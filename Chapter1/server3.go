3fc8d0985f5e7e5e83871965472d3b531b896e64
Patrick Abele <janpatrix@gmail.com>
2023-11-03 08:22:06 +0100

    added server3

---
+++ Chapter1/server3.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"sync"
+)
+
+var mu sync.Mutex
+var count int
+
+func main() {
+	http.HandleFunc("/", anotherHandler)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+
+}
+
+func anotherHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "%s %s %s\n", r.Method, r.URL, r.Proto)
+	for k, v := range r.Header {
+		fmt.Fprintf(w, "Header[%q]\n = %q\n", k, v)
+	}
+	fmt.Fprintf(w, "Host = %q\n", r.Host)
+	fmt.Fprintf(w, "RemoteAddr = %q\n", r.RemoteAddr)
+	if err := r.ParseForm(); err != nil {
+		log.Print(err)
+	}
+	for k, v := range r.Form {
+		fmt.Fprintf(w, "Form[%q] = %q\n", k, v)
+	}
+}
+
+// kill -9 $(lsof -ti:8000)
