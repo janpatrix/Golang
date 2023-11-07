17662f16ce43c2e1feed580cbbb89c0802b607cb
Patrick Abele <patrick.abele@Patricks-MacBook-Air.local>
2023-11-07 19:26:26 +0100

    added customer error handlers

--- greenlight/cmd/api/main.go
+++ greenlight/cmd/api/main.go
@@ -49,5 +49,4 @@ func main() {
 	logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
 	err := srv.ListenAndServe()
 	logger.Fatal(err)
-
 }
--- greenlight/cmd/api/movies.go
+++ greenlight/cmd/api/movies.go
@@ -15,7 +15,7 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
 	id, err := app.readIDParam(r)
 	if err != nil {
-		http.NotFound(w, r)
+		app.notFoundResponse(w, r)
 		return
 	}
 
@@ -30,8 +30,6 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 
 	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
 	if err != nil {
-		app.logger.Println(err)
+		app.serverErrorResponse(w, r, err)
 	}
-
-	fmt.Fprintf(w, "show the details of movie %d\n", id)
 }
