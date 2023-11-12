448d5bd08c4903f4e4821e3454dc8e5fc2c3894b
Patrick Abele <janpatrix@gmail.com>
2023-11-12 19:13:56 +0100

    some refactoring and add routes.go

--- src/github.com/engineeringleader/lets_go/snippetbox/cmd/web/helpers.go
+++ src/github.com/engineeringleader/lets_go/snippetbox/cmd/web/helpers.go
@@ -19,7 +19,7 @@ func neuter(next http.Handler) http.Handler {
 
 func (app *application) serverError(w http.ResponseWriter, err error) {
 	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
-	app.errorLog.Println(trace)
+	app.errorLog.Output(2, trace)
 
 	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 }
