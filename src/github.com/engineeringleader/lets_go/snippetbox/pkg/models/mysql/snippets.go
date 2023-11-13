c2b1e624c1a364d7fddb4d241784ca111b1e563e
Patrick Abele <patrick.abele@Patricks-MacBook-Air.local>
2023-11-13 10:33:19 +0100

    changes

--- src/github.com/engineeringleader/lets_go/snippetbox/pkg/models/mysql/snippets.go
+++ src/github.com/engineeringleader/lets_go/snippetbox/pkg/models/mysql/snippets.go
@@ -2,6 +2,7 @@ package mysql
 
 import (
 	"database/sql"
+	"errors"
 
 	"engineeringleader.de/snippetbox/snippetbox/pkg/models"
 	// Import the models package that we just created. You need to prefix this with
@@ -34,7 +35,21 @@ func (m *SnippetModel) Insert(title, content, expires string) (int, error) {
 
 // This will return a specific snippet based on its id.
 func (m *SnippetModel) Get(id int) (*models.Snippet, error) {
-	return nil, nil
+	stmt := `SELECT id, title, content, created, expires FROM snippets
+	WHERE expires > UTC_TIMESTAMP() AND id = ?`
+
+	row := m.DB.QueryRow(stmt, id)
+	s := &models.Snippet{}
+
+	err := row.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, models.ErrNoRecord
+		} else {
+			return nil, err
+		}
+	}
+	return s, nil
 }
 
 // This will return the 10 most recently created snippets.
