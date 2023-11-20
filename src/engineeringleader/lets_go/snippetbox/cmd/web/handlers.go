f19e9b1354275b45722585320c3ff0bea2444828
Patrick Abele <janpatrix@gmail.com>
2023-11-20 17:31:08 +0100

    added more middleware functions

--- src/engineeringleader/lets_go/snippetbox/cmd/web/handlers.go
+++ src/engineeringleader/lets_go/snippetbox/cmd/web/handlers.go
@@ -41,8 +41,11 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	flash := app.session.PopString(r, "flash")
+
 	app.render(w, r, "show.page.tmpl", &templateData{
 		Snippet: s,
+		Flash:   flash,
 	})
 }
 
@@ -75,6 +78,8 @@ func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 		app.serverError(w, err)
 		return
 	}
+
+	app.session.Put(r, "flash", "Snippet successfully created!")
 
 	http.Redirect(w, r, fmt.Sprintf("/snippet/%d", id), http.StatusSeeOther)
 }
