7c816d3948fd8dd56cef841e28e8088d19b88323
Patrick Abele <janpatrix@gmail.com>
2023-11-21 20:15:47 +0100

    added logout

--- src/engineeringleader/lets_go/snippetbox/cmd/web/handlers.go
+++ src/engineeringleader/lets_go/snippetbox/cmd/web/handlers.go
@@ -81,7 +81,9 @@ func (app *application) loginUser(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) logoutUser(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Log the user out...")
+	app.session.Remove(r, "authenticatedUserID")
+	app.session.Put(r, "flash", "You have been logged out successfully!")
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
