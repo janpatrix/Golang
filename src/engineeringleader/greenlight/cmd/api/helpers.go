2b9ea1b6cd362a3bd0f2deddb7a24b60f6d668e2
Patrick Abele <patrick.abele@9afbc5f9-eb6a-4ea4-a350-b0f70c36543c.fritz.box>
2023-12-03 10:16:16 +0100

    more error handlers

--- src/engineeringleader/greenlight/cmd/api/helpers.go
+++ src/engineeringleader/greenlight/cmd/api/helpers.go
@@ -47,7 +47,14 @@ func (app *application) writeJSON(w http.ResponseWriter, status int, data interf
 
 func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst interface{}) error {
 
-	err := json.NewDecoder(r.Body).Decode(dst)
+	maxBytes := 1_048_576
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
+
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields()
+
+	err := dec.Decode(dst)
+
 	if err != nil {
 		var syntaxError *json.SyntaxError
 		var unmarshalTypeError *json.UnmarshalTypeError
