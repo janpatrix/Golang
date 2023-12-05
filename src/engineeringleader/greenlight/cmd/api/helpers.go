2f65f30b418a30a9a0774e84f2e8a6a96484ef80
janpatrix <janpatrix@gmail.com>
2023-12-05 18:05:32 +0100

    added JSON validator

--- src/engineeringleader/greenlight/cmd/api/helpers.go
+++ src/engineeringleader/greenlight/cmd/api/helpers.go
@@ -85,5 +85,10 @@ func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst int
 
 	}
 
+	err = dec.Decode(&struct{}{})
+	if err != io.EOF {
+		return errors.New("body must only contain a single JSON value")
+	}
+
 	return nil
 }
