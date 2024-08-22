eb935259aef88d1c586d1b8058b0f498c51457fc
janpatrix <janpatrix@gmail.com>
2024-08-22 21:35:30 +0200

    add kube project

---
+++ src/engineeringleader/go-devops/kube-go/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+)
+
+func main() {
+
+	var (
+		client *kubernetes.Clientset
+		err    error
+	)
+
+	client, err = getClient()
+	if err != nil {
+		fmt.Printf("error getting the kube client: %s", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Got Kube client successful: %v", client)
+}
+
+func getClient() (*kubernetes.Clientset, error) {
+
+	// use the current context in kubeconfig
+	config, err := clientcmd.BuildConfigFromFlags("", filepath.Join(homedir.HomeDir(), ".kube", "config"))
+	if err != nil {
+		return nil, err
+	}
+
+	// create the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+
+	return clientset, nil
+}
